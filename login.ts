2e320befa70606e1b808078f6635af870153e87e
emiliosp <emilio.spatola@gmail.com>
2025-05-16 16:00:18 +0200

    chore: documentation.json

--- login.ts
+++ login.ts
@@ -1,8 +1,9 @@
 import { writeFileSync } from 'node:fs';
 import tough from 'tough-cookie';
+import { createHash } from 'node:crypto';
 
 interface AuthResponse {
-  authcode: string;
+  authcode: string | 0;
 }
 
 interface AuthToken {
@@ -14,6 +15,11 @@ if (!process.env.EMAIL || !process.env.PASSWORD || !process.env.COOKIE_JAR) {
   throw new Error('Required environment variables are not set');
 }
 
+// // https://forums.iracing.com/discussion/15068/general-availability-of-data-api/p1
+// console.log(createHash('sha256').update(`${password}${email}`).digest('base64'));
+// process.exit(1);
+// remember to enable legacy authentication in your account settings
+
 const response = await fetch('https://members-ng.iracing.com/auth', {
   method: 'POST',
   headers: {
@@ -21,11 +27,15 @@ const response = await fetch('https://members-ng.iracing.com/auth', {
   },
   body: JSON.stringify({
     email: process.env.EMAIL,
-    password: process.env.PASSWORD,
+    password: createHash('sha256').update(`${process.env.PASSWORD}${process.env.EMAIL}`).digest('base64'),
   }),
 });
 
 const { authcode } = await response.json() as AuthResponse;
+
+if (authcode === 0) {
+  throw new Error('Failed to login');
+}
 
 const cookieHeader = response.headers.get('set-cookie');
 if (!cookieHeader) {
